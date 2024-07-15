77f3ede571e9cec42ba4ad4116d64fad3930314a
voskreseniev <roman.mayber@gmail.com>
2024-07-15 10:26:50 +0300

    +

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const express = require('express');
-const axios = require('axios');
 const dotenv = require('dotenv');
 const { DateTime } = require('luxon');
 const winston = require('winston');
 const TelegramBot = require('node-telegram-bot-api');
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
+const { createClient } = require('@supabase/supabase-js');
 
-dotenv.config();  // Загружаем переменные окружения из .env
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 
-// Инициализация Telegram Bot
 const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
 
-// Настройка логгера
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
@@ -30,28 +26,10 @@ const logger = winston.createLogger({
     ],
 });
 
-// Инициализация базы данных
-const dbPath = path.resolve(__dirname, 'light_states.db');
-const db = new sqlite3.Database(dbPath, (err) => {
-    if (err) {
-        logger.error(`Ошибка подключения к базе данных: ${err.message}`);
-    } else {
-        logger.info('Успешное подключение к базе данных');
-    }
-});
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-// Создание таблицы, если её ещё нет
-db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS light_states (
-        chat_id TEXT PRIMARY KEY,
-        last_ping_time TEXT,
-        light_state TEXT,
-        light_start_time TEXT,
-        previous_duration TEXT
-    )`);
-});
-
-// Функция для отправки сообщений в Telegram
 function sendTelegramMessage(chatId, message) {
     bot.sendMessage(chatId, message)
         .then(() => {
@@ -62,89 +40,95 @@ function sendTelegramMessage(chatId, message) {
         });
 }
 
-// Функция для сохранения состояния света в базу данных
-function saveLightState(chatId, lastPingTime, lightState, lightStartTime, previousDuration) {
-    db.run(`INSERT OR REPLACE INTO light_states (chat_id, last_ping_time, light_state, light_start_time, previous_duration) 
-            VALUES (?, ?, ?, ?, ?)`,
-        [chatId, lastPingTime.toISO(), lightState, lightStartTime.toISO(), previousDuration ? previousDuration.toISO() : null],
-        (err) => {
-            if (err) {
-                logger.error(`Ошибка сохранения данных в базу: ${err.message}`);
-            } else {
-                logger.info(`Данные для chat_id ${chatId} сохранены в базу`);
-            }
+async function saveLightState(chatId, lastPingTime, lightState, lightStartTime, previousDuration) {
+    const formattedLastPingTime = lastPingTime.toISO();  // Преобразуем в ISO формат для сохранения
+    const formattedLightStartTime = lightStartTime.toISO();  // Аналогично
+
+    let formattedPreviousDuration = null;
+    if (previousDuration) {
+        formattedPreviousDuration = previousDuration.toFormat("hh:mm:ss"); // Преобразуем в строку формата hh:mm:ss
+    }
+
+    const { error } = await supabase
+        .from('light_states')
+        .upsert({
+            chat_id: chatId,
+            last_ping_time: formattedLastPingTime,
+            light_state: lightState,
+            light_start_time: formattedLightStartTime,
+            previous_duration: formattedPreviousDuration // Храним как текст
         });
+
+    if (error) {
+        logger.error(`Ошибка сохранения данных в базу: ${error.message}`);
+    } else {
+        logger.info(`Данные для chat_id ${chatId} сохранены в базу`);
+    }
 }
 
-// Функция для получения состояния света из базы данных
-function getLightState(chatId, callback) {
-    db.get(`SELECT * FROM light_states WHERE chat_id = ?`, [chatId], (err, row) => {
-        if (err) {
-            logger.error(`Ошибка чтения данных из базы: ${err.message}`);
-            callback(err);
-        } else {
-            callback(null, row);
-        }
-    });
+async function getLightState(chatId) {
+    const { data, error } = await supabase
+        .from('light_states')
+        .select('*')
+        .eq('chat_id', chatId)
+        .single();
+
+    if (error) {
+        logger.error(`Ошибка чтения данных из базы: ${error.message}`);
+        return null;
+    }
+    return data;
 }
 
-// Функция для обновления времени последнего пинга
-function updatePingTime(chatId) {
+async function updatePingTime(chatId) {
     const now = DateTime.now();
     logger.info(`Получен пинг от ${chatId}`);
 
-    getLightState(chatId, (err, row) => {
-        if (err) {
-            logger.error(`Ошибка получения данных для chat_id ${chatId}`);
-            return;
-        }
-
-        if (!row) {
-            const lightState = "включен";
-            const lightStartTime = now;
-            saveLightState(chatId, now, lightState, lightStartTime, null);
-            sendTelegramMessage(chatId, `Привет! Текущий статус света: ${lightState}`);
+    const row = await getLightState(chatId);
+    if (!row) {
+        await saveLightState(chatId, now, true, now, null);
+        sendTelegramMessage(chatId, `Привет! Свет включен.`);
+    } else {
+        const lastState = row.light_state;  // true/false
+        const lightStartTime = DateTime.fromISO(row.light_start_time);
+        if (!lastState) {  // если свет выключен
+            const previousDuration = now.diff(lightStartTime);
+            await saveLightState(chatId, now, true, now, previousDuration);
+            sendTelegramMessage(chatId, `Свет ВКЛЮЧИЛИ. Был выключен на протяжении ${previousDuration.toFormat('hh:mm:ss')}.`);
+            logger.info(`Свет включен для ${chatId}. Выключен на протяжении ${previousDuration.toFormat('hh:mm:ss')}.`);
         } else {
-            const lastState = row.light_state;
-            const lightStartTime = DateTime.fromISO(row.light_start_time);
-            if (lastState === "выключен") {
-                const previousDuration = now.diff(lightStartTime);
-                saveLightState(chatId, now, "включен", now, previousDuration);
-                sendTelegramMessage(chatId, `Свет ВКЛЮЧИЛИ. Был выключен на протяжении ${previousDuration.toFormat('hh:mm:ss')}.`);
-                logger.info(`Свет включен для ${chatId}. Выключен на протяжении ${previousDuration.toFormat('hh:mm:ss')}.`);
-            } else {
-                saveLightState(chatId, now, "включен", lightStartTime, null);
-            }
+            // Если свет уже включен, просто обновляем время последнего пинга
+            await saveLightState(chatId, now, lastState, lightStartTime, null);
         }
-    });
+    }
 }
 
-// Определяем интервал проверки состояния света (3 минуты)
-const lightCheckInterval = 180000; // 3 минуты в миллисекундах
+const lightCheckInterval = 30000; // 10 секунд
 
-// Фоновая задача для проверки состояния света
-setInterval(() => {
+setInterval(async () => {
     const now = DateTime.now();
-    db.all(`SELECT * FROM light_states`, [], (err, rows) => {
-        if (err) {
-            logger.error(`Ошибка чтения данных из базы: ${err.message}`);
-            return;
-        }
+    const { data: rows, error } = await supabase
+        .from('light_states')
+        .select('*');
 
-        rows.forEach((row) => {
-            const chatId = row.chat_id;
-            const lastPingTime = DateTime.fromISO(row.last_ping_time);
-            if (now.diff(lastPingTime).as('seconds') > 180) {  // 3 минуты
-                if (row.light_state === "включен") {
-                    const lightStartTime = DateTime.fromISO(row.light_start_time);
-                    const previousDuration = now.diff(lightStartTime);
-                    saveLightState(chatId, now, "выключен", now, previousDuration);
-                    sendTelegramMessage(chatId, `Свет ВЫКЛЮЧИЛИ. Был включен на протяжении ${previousDuration.toFormat('hh:mm:ss')}.`);
-                    logger.info(`Свет выключен для ${chatId}. Включен на протяжении ${previousDuration.toFormat('hh:mm:ss')}.`);
-                }
+    if (error) {
+        logger.error(`Ошибка чтения данных из базы: ${error.message}`);
+        return;
+    }
+
+    for (const row of rows) {
+        const chatId = row.chat_id;
+        const lastPingTime = DateTime.fromISO(row.last_ping_time);
+        if (now.diff(lastPingTime).as('seconds') > 180) {  // Период проверки в секундах
+            if (row.light_state) {  // если свет включен
+                const lightStartTime = DateTime.fromISO(row.light_start_time);
+                const previousDuration = now.diff(lightStartTime);
+                await saveLightState(chatId, now, false, now, previousDuration);
+                sendTelegramMessage(chatId, `Свет ВЫКЛЮЧИЛИ. Был включен на протяжении ${previousDuration.toFormat('hh:mm:ss')}.`);
+                logger.info(`Свет выключен для ${chatId}. Включен на протяжении ${previousDuration.toFormat('hh:mm:ss')}.`);
             }
-        });
-    });
+        }
+    }
 }, lightCheckInterval);
 
 app.post('/ping', (req, res) => {
@@ -159,34 +143,31 @@ app.get('/ping', (req, res) => {
     res.send("Ping received!");
 });
 
-// Обработчик команды /status
-bot.onText(/\/status/, (msg) => {
+bot.onText(/\/status/, async (msg) => {
     const chatId = msg.chat.id;  
     logger.info(`Команда /status получена от группы с chat_id ${chatId}`);
 
-    getLightState(chatId, (err, row) => {
-        if (err || !row) {
-            const message = `Данных для chat_id ${chatId} не найдено.`;
-            bot.sendMessage(chatId, message);
-            logger.warn(message);
-            return;
-        }
+    const row = await getLightState(chatId);
+    if (!row) {
+        const message = `Данных для chat_id ${chatId} не найдено.`;
+        bot.sendMessage(chatId, message);
+        logger.warn(message);
+        return;
+    }
 
-        const lightState = row.light_state;
-        const durationCurrent = DateTime.now().diff(DateTime.fromISO(row.light_start_time));
-        const previousDuration = row.previous_duration ? DateTime.fromISO(row.previous_duration).toFormat('hh:mm:ss') : 'неизвестно';
-        const responseMessage = `Свет ${lightState} на протяжении ${durationCurrent.toFormat('hh:mm:ss')}. Предущий статус длился ${previousDuration}.`;
+    const lightState = row.light_state ? 'включен' : 'выключен';
+    const durationCurrent = DateTime.now().diff(DateTime.fromISO(row.light_start_time));
+    const previousDuration = row.previous_duration || 'неизвестно';
+    const responseMessage = `Свет ${lightState} на протяжении ${durationCurrent.toFormat('hh:mm:ss')}. Предыдущий статус длился ${previousDuration}.`;
 
-        bot.sendMessage(chatId, responseMessage);
-        logger.info(`Статус отправлен для группы ${chatId}`);
-    });
+    bot.sendMessage(chatId, responseMessage);
+    logger.info(`Статус отправлен для группы ${chatId}`);
 });
 
-// Запуск сервера
 const PORT = process.env.PORT || 5002;
 const server = app.listen(PORT, () => {
     const address = server.address();
-    const host = address.address === '::' ? 'localhost' : address.address;  // Проверяем, если адрес "::" (IPv6), заменяем на "localhost"
+    const host = address.address === '::' ? 'localhost' : address.address;
     const port = address.port;
     const welcomeChatId = '558625598';  // Укажите нужный chat_id
     sendTelegramMessage(welcomeChatId, `Привет! Бот запущен и готов к работе на адресе http://${host}:${port}`);
