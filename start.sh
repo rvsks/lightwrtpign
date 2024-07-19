d5ca4203a030985b1e83d679d17ba6c1b72f54d0
voskreseniev <roman.mayber@gmail.com>
2024-07-19 20:52:10 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Установка chromedriver
-wget -N https://chromedriver.storage.googleapis.com/109.0.5414.74/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip -d /usr/local/bin/
-chmod +x /usr/local/bin/chromedriver
+# Установка chromedriver в /tmp
+wget -N https://chromedriver.storage.googleapis.com/109.0.5414.74/chromedriver_linux64.zip -P /tmp/
+unzip /tmp/chromedriver_linux64.zip -d /tmp/
+chmod +x /tmp/chromedriver
+
+# Убедитесь, что путь к chromedriver правильный в bot.py
+sed -i 's|/app/chromedriver/chromedriver|/tmp/chromedriver|' bot.py
 
 # Запуск бота
 python bot.py
